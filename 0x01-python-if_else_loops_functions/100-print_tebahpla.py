c70aaab3c09d37a22ccf24a56ad11accc18369f1
Martin Nyaga <martinnyaga7373@gmail.com>
2022-06-30 07:52:02 -0700

    task 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,6 +2,6 @@
 for i in range(97, 123):
 
     if i % 2 == 0:
-        print(chr(i), end='')
+        print('{}'.format(chr(i)), end='')
     if i % 2 == 1:
-        print(chr(i).upper(), end='')
+        print('{}'.format(chr(i).upper()), end='')
