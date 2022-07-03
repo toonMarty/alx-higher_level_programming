5876f3232ddd0d23bcffa6100ce45e71bd8fcb27
Martin Nyaga <martinnyaga7373@gmail.com>
2022-07-03 07:40:03 -0700

    task 100

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) != 4:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+    else:
+        if sys.argv[2] == '+':
+            print(f'{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {add(int(sys.argv[1]), int(sys.argv[3]))}')
+        elif sys.argv[2] == '*':
+            print(f'{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {mul(int(sys.argv[1]), int(sys.argv[3]))}')
+        elif sys.argv[2] == '-':
+            print(f'{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {sub(int(sys.argv[1]), int(sys.argv[3]))}')
+        elif sys.argv[2] == '/':
+            print(f'{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {div(int(sys.argv[1]), int(sys.argv[3]))}')
+        else:
+            print('Unknown operator. Available operators are *,+, /, -')
+            exit(1)
