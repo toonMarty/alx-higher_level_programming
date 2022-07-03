1b82832c70ef0c314b83baf7cb9c77ad146b3b6a
Martin Nyaga <martinnyaga7373@gmail.com>
2022-07-03 07:29:03 -0700

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,8 @@ if __name__ == '__main__':
 
     if len(sys.argv) == 1:
         print('{} arguments.'.format(len(sys.argv) - 1))
+    elif len(sys.argv) == 2:
+        print('{} argument:'.format(len(sys.argv) - 1))
     else:
         print('{} arguments:'.format(len(sys.argv) - 1))
 
