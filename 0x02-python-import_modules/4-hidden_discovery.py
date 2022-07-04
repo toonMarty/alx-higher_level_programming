09727b6d59ccd31d0053e2a0959e6a7c6cd63c7d
Martin Nyaga <martinnyaga7373@gmail.com>
2022-07-04 08:44:45 -0700

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-        import hidden_4
-        a = dir(hidden_4)
-        for i in range(9,12):
-                print(a[i])
+    import hidden_4
+    a = dir(hidden_4)
+    for i in range(len(dir(hidden_4))):
+        if not a[i].__contains__('__'):
+            print(a[i])
