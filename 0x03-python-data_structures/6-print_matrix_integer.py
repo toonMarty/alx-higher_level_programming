7b763ffc2ba11f7f403a286572890efbc72bf02f
Martin Nyaga <martinnyaga7373@gmail.com>
2022-07-04 09:15:36 -0700

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
-        print('{}'.format(matrix[i]).replace(',', '', 1000000).
+        print('{:d}'.format(matrix[i]).replace(',', '', 1000000).
               replace('[', '', 1000000).
               replace(']', '', 1000000))
