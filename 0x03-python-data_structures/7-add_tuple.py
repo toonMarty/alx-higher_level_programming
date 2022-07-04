fe4c0ce2dc29e8de464b346a35e2037645cf5413
Martin Nyaga <martinnyaga7373@gmail.com>
2022-07-04 10:06:23 -0700

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2 and len(tuple_b) < 2:
+    if len(tuple_a) == 0 and len(tuple_b) == 0:
+        return 0, 0
+
+    elif len(tuple_a) == 0 and len(tuple_b) < 2:
+        for i in range(len(tuple_b)):
+            new_tuple = (int(tuple_b[i]), 0)
+            return new_tuple
+
+    elif len(tuple_b) == 0 and len(tuple_a) < 2:
+        for i in range(len(tuple_a)):
+            new_tuple = (int(tuple_a[i]), 0)
+            return new_tuple
+
+    elif len(tuple_a) < 2 and len(tuple_b) < 2:
         for i in range(len(tuple_a)):
             for j in range(len(tuple_b)):
                 new_tuple = (int(tuple_a[i]) + int(tuple_b[j]), 0)
@@ -29,9 +42,3 @@ def add_tuple(tuple_a=(), tuple_b=()):
                 new_tuple = (int(tuple_a[i]) + int(tuple_b[j]),
                              int(tuple_a[i + 1]) + 0)
                 return new_tuple
-
-    for i in range(len(tuple_a)):
-        for j in range(len(tuple_b)):
-            new_tuple = (int(tuple_a[i]) + int(tuple_b[j]),
-                         int(tuple_a[i + 1]) + int(tuple_b[i + 1]))
-            return new_tuple
