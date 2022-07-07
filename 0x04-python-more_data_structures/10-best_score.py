ac3e0da99e0272a996272fb85de46d5df67ce007
Martin Nyaga <martinnyaga7373@gmail.com>
2022-07-06 17:34:46 -0700

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == {}:
+    if a_dictionary is None:
         return None
     else:
         key_sort = list(a_dictionary.values())
         key_sort.sort()
         a = key_sort[len(key_sort) - 1]
-
         for key in a_dictionary:
             if a == a_dictionary[key]:
                 return key
