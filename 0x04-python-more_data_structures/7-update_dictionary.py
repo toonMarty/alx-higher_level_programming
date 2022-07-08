20e6fb7c805ba19516091e676d31b1196dddf166
Martin Nyaga <martinnyaga7373@gmail.com>
2022-07-08 10:25:31 -0700

    task 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if key in a_dictionary:
+        a_dictionary[key] = value
+        return a_dictionary
+    if key not in a_dictionary:
+        a_dictionary.update({key: value})
+        return a_dictionary
