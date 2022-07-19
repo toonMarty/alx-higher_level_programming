644cb639b43bb08b21b540d09776811992fa214f
Martin Nyaga <martinnyaga7373@gmail.com>
2022-07-19 11:26:26 -0700

    task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""This module contains an empty class Square
+"""
+
+
 class Square:
-    """an empty class Square that defines a square"""
+    """an empty class Square that defines a square
+    """
     pass
