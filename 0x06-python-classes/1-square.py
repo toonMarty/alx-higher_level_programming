d1e92efbf14a0b78da3575a474f60bb354bb4261
Martin Nyaga <martinnyaga7373@gmail.com>
2022-07-19 11:49:05 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module contains a class that defines a
+    square and instantiates size of the square
+"""
+
+
+class Square:
+    """A class that defines a square with a size
+    """
+
+    def __init__(self, __x):
+        """This method initializes by passing
+            initial values to the square object
+        """
+        self.__x = __x
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -1,4 +1,4 @@
 Task 0: An empty class square that defines a square
-Task 1:
+Task 1: A class square that defines a square with size
 Task 2:
 Task 3:
