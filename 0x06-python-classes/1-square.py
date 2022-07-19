3e9795a7a2f89e1aab30974b0e2a2365e73f0859
Martin Nyaga <martinnyaga7373@gmail.com>
2022-07-19 11:55:59 -0700

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,8 +8,11 @@ class Square:
     """A class that defines a square with a size
     """
 
-    def __init__(self, __x):
+    def __init__(self, __size):
         """This method initializes by passing
             initial values to the square object
+
+            Args:
+                __size: size of square
         """
-        self.__x = __x
+        self.__size = __size
