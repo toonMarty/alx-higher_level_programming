400ff7af81f1f7620909d6d9cc5a2dfee42b59b2
Martin Nyaga <martinnyaga7373@gmail.com>
2022-07-27 14:39:27 -0700

    task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module contains an empty class that defines a rectangle
+"""
+
+
+class Rectangle:
+    """an empty class rectangle that defines a rectangle
+    """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,10 @@
+This file contains more tasks on classes and objects
+Task 0: An empty class Rectangle that defines a rectangle
+Task 1:
+Task 2:
+Task 3:
+Task 4:
+Task 5:
+Task 6:
+Task 7:
+Task 8:
