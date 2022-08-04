15add2b3241f956c3bebc8f7f0f2998c36b149e8
Martin Nyaga <martinnyaga7373@gmail.com>
2022-08-04 06:37:48 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module contains an empty class BaseGeometry
+and a method area()
+"""
+
+
+class BaseGeometry:
+    """a class that defines BaseGeometry
+    """
+    def area(self):
+        """
+        This method checks whether area has been implemented
+        """
+        raise Exception('area() is not implemented')
