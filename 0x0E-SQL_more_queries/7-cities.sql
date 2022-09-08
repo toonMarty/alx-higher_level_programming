7a5958d80eb9ca0e597fe657c7013220f4155213
toonMarty <martinnyaga7373@gmail.com>
2022-09-08 15:47:58 -0700

    task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,10 +1,11 @@
 -- a script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-use hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities
 (
-        id INTEGER UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+        id INTEGER AUTO_INCREMENT NOT NULL UNIQUE,
         name VARCHAR(256) NOT NULL,
         state_id INTEGER NOT NULL,
+        CONSTRAINT prim_cities PRIMARY KEY(id),
         CONSTRAINT foreign_cities FOREIGN KEY(state_id) REFERENCES states(id)
 )ENGINE=InnoDB;
