8f0ce2213313c3b0d6b83b1aa11c1cb5162ecfe5
toonMarty <martinnyaga7373@gmail.com>
2022-09-08 15:51:50 -0700

    task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -8,4 +8,4 @@ CREATE TABLE IF NOT EXISTS cities
         state_id INTEGER NOT NULL,
         CONSTRAINT prim_cities PRIMARY KEY(id),
         CONSTRAINT cities_ibfk_1 FOREIGN KEY(state_id) REFERENCES states(id)
-)ENGINE=InnoDB;
+);
