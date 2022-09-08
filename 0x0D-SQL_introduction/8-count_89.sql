9cf7468b4541333bb5bea627d146082809887754
Martin Nyaga <martinnyaga7373@gmail.com>
2022-09-08 13:03:04 -0700

    task 8

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,4 @@
 -- a script that displays the number of records with id = 89 in the table first_table in MySQL server
-SELECT COUNT(id = 89) FROM first_table;
+SELECT COUNT(id)
+	FROM first_table
+	WHERE id = 89;
