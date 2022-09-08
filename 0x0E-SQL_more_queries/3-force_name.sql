e969cb18cb2ad53431505be31b142e5e3caa7cee
Martin Nyaga <martinnyaga7373@gmail.com>
2022-09-07 17:04:45 -0700

    task 3

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,6 +1,7 @@
 -- a script that creates the table force_name on mySQL server
 USE hbtn_0d_2;
-CREATE TABLE IF NOT EXISTS force_name(
+CREATE TABLE IF NOT EXISTS force_name
+(
         id INTEGER,
         name VARCHAR(256) NOT NULL
-        );
+);
