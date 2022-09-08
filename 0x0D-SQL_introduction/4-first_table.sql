1186a8ce005ce154506981fdbd111754d0d45f91
Martin Nyaga <martinnyaga7373@gmail.com>
2022-09-08 12:57:38 -0700

    tasks 3-16

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- a script that creates a table called first_table in the current database in MySQL server
+CREATE TABLE IF NOT EXISTS first_table
+(
+        id INTEGER,
+        name VARCHAR(256)
+);
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- a script that displays the number of records with id = 89 in the table first_table in MySQL server
+SELECT COUNT(id = 89) FROM first_table;
