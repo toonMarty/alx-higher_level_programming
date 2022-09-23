370a3e5210107104de7b728ae0a6faee77a34dae
toonMarty <martinnyaga7373@gmail.com>
2022-09-23 15:06:01 -0700

    Rotten Tomatoes

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- a script that lists all shows from hbtn_0d_tvshows_rate by their rating.
 SELECT `title`, SUM(`rate`) AS `rating`
-	FROM `tv_shows AS t
+	FROM `tv_shows` AS t
 		INNER JOIN `tv_show_ratings` AS r
 		ON t.`id` = r.`show_id`
 	GROUP BY `title`
