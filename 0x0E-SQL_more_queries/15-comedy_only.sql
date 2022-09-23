e15f055bd1d138d5538da56b624916212078073f
toonMarty <martinnyaga7373@gmail.com>
2022-09-23 14:51:40 -0700

    Only Comedy

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
 -- a script that lists all Comedy shows in the database hbtn_0d_tvshows.
 SELECT t.`title`
 	FROM `tv_shows AS t
-	INNER JOIN `tv_show_genres AS s
+	INNER JOIN `tv_show_genres` AS s
 	ON t.`id` = s.`show_id`
 
 	INNER JOIN `tv_genres` AS g
