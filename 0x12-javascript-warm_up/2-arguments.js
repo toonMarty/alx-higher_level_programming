cf2e6d0524a10aa5c3aa6865e208d984d629d7dd
toonMarty <martinnyaga7373@gmail.com>
2022-10-03 17:48:14 -0700

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-if (process.argv.length === 3) {
+if (process.argv.length == 3) {
   console.log('Argument found');
-} else if (process.argv.length === 2) {
+} else if (process.argv.length == 2) {
   console.log('No argument');
 } else {
   console.log('Arguments found');
