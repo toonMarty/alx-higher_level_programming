d014574a6ac37fe1f956d19cdd84021bc9861e8f
toonMarty <martinnyaga7373@gmail.com>
2022-10-03 17:59:14 -0700

    Loop to Languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const langArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const langArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let idx;
 
 for (idx = 0; idx < langArray.length; idx++) {
