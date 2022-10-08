c30ba07711fc293ecd1ce31ee3c811c0a9667d5a
toonMarty <martinnyaga7373@gmail.com>
2022-10-07 23:16:44 -0700

    Cities in state

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -19,4 +19,3 @@ class City(Base):
                 nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-
