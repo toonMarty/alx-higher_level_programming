1bb467a803bf052eb30697911d167d044e3de29a
toonMarty <martinnyaga7373@gmail.com>
2022-10-23 14:11:12 -0700

    Status #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     import requests
     r = requests.get('https://alx-intranet.hbtn.io/status')
     print('Body response:')
-    print('\t- type: {}'.format(type(r)))
+    print('\t- type: {}'.format(type(r.text)))
     print('\t- content: {}'.format(r.text))
