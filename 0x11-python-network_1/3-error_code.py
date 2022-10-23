c1e42140b732162117fbc98c20a090c10ac54e97
toonMarty <martinnyaga7373@gmail.com>
2022-10-23 13:12:37 -0700

    Error codes

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a Python script that takes in a URL, sends a request to the URL and
+displays the body of the response
+(decoded in utf-8)"""
+if __name__ == "__main__":
+    from urllib.request import Request, urlopen
+    from urllib.parse import urlencode
+    import sys
+    import urllib
+
+    req = Request(sys.argv[1])
+
+    try:
+        response = urlopen(req).read().decode('utf-8')
+        print(response)
+    except urllib.error.HTTPError as e:
+        print('Error code:', e.code)
