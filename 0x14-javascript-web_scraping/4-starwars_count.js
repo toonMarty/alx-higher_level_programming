eec0a7cf9953ddc62c74168e641df0c928e96c82
toonMarty <martinnyaga7373@gmail.com>
2022-11-15 23:47:28 +0000

    Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const film in films) {
+      const fChars = films[film].characters;
+      for (const idx in fChars) {
+        if (fChars[idx].includes('18')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
+  }
+});
