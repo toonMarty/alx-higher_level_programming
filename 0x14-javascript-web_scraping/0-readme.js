925323cb1787a9fbb488d50a63b8d1ec02d1c27c
toonMarty <martinnyaga7373@gmail.com>
2022-11-15 19:52:18 +0000

    Read file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+Error.stackTraceLimit = 0
+
+try {
+  const fs = require('fs'); // Load the fs API
+  const text = fs.readFileSync(process.argv[2], 'utf8');
+  console.log(text);
+} catch (e) {
+  console.log(e);
+}
