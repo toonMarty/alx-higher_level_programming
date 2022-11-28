312b316646e50d44c6fe9486c72c7d02eef1d9b8
toonMarty <martinnyaga7373@gmail.com>
2022-11-28 19:05:37 +0000

    No JQuery

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,6 @@
+/*
+a JavaScript script that updates the text color
+of the <header> element to red (#FF0000)
+ */
+const formatHeader = document.querySelector('header');
+formatHeader.style.color = '#FF0000';
