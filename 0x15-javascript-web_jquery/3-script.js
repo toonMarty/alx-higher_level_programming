57a883e6be31e91dd826abdbae0239e72eedb975
toonMarty <martinnyaga7373@gmail.com>
2022-11-28 19:20:06 +0000

    Add .red class

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,9 @@
+/*
+ a JavaScript script that adds the class red to the
+ <header> element when the user clicks on the tag
+ DIV#red_header
+ */
+
+$('DIV#red_header').on('click', function () {
+  $('header').addClass('red');
+});
