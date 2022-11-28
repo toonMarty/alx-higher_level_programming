2d7c3a9b9c0d6c3b8b07f73e29e133bc6afb7817
toonMarty <martinnyaga7373@gmail.com>
2022-11-28 19:33:31 +0000

    Star wars character

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,10 @@
+/*
+a JavaScript script that fetches the character name from this
+URL: https://swapi-api.hbtn.io/api/people/5/?format=json
+ */
+
+const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
+
+$.get(url, function (data) {
+  $('DIV#character').text(data.name);
+});
