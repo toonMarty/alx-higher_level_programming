2b8564bf001f09c8e20c29947ecd932909aa6b0d
toonMarty <martinnyaga7373@gmail.com>
2023-01-13 12:51:10 +0000

    From City

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -19,4 +19,3 @@ if __name__ == "__main__":
     for city in session.query(City).order_by(City.id).all():
         print("{}: {} -> {}".format(city.id, city.name, city.state.name))
     session.close()
-
